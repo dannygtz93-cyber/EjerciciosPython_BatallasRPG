62b680c088e08848a1e480ee630398ce5d5b94a0
dannygtz93-cyber <dannygtz93@gmail.com>
2026-03-31 22:20:04 -0600

    Agrego clase base Personaje

--- BatallasRPG.py
+++ BatallasRPG.py
@@ -9,7 +9,7 @@
 #==============================#
 
 TITULO = "Batallas RPG"
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 
 #==============================#
 # FUNCIONES DE INTERFAZ (UI)
@@ -21,3 +21,25 @@ def impr_titulo_version():
 #==============================#
 # CLASSES
 #==============================#
+
+class Personaje:
+  
+  def __init__(self, nombre, nivel, vida, vida_max, ataque, defensa):
+    self.nombre = nombre
+    self._nivel = nivel
+    self._vida = vida
+    self._vida_max = vida_max
+    self._ataque = ataque
+    self._defensa = defensa
+
+  def atacar(self):
+    pass
+  
+  def recibir_daño(self):
+    pass
+
+  def esta_vivo(self):
+    pass
+
+  def subir_nivel(self):
+    pass
